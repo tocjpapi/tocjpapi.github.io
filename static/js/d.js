90b617389373ad469783188a90efa80e178c790e
CJ <treasure32109@gmail.com>
2025-04-04 12:16:44 +0100

    hhhhh

---
+++ static/js/d.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a').forEach(link => {
+    link.setAttribute('draggable', 'false');
+  });
+
+
+  document.querySelectorAll('img').forEach(img => {
+    img.setAttribute('draggable', 'false');
+  });
+
